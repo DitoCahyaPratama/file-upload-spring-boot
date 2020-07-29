95666d1d72f7808054714e2124807e304ea5e22c
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-30 03:49:30 +0700

    Enable configuration properties

--- src/main/java/com/example/fileuploadspringboot/FileUploadSpringBootApplication.java
+++ src/main/java/com/example/fileuploadspringboot/FileUploadSpringBootApplication.java
@@ -1,9 +1,15 @@
 package com.example.fileuploadspringboot;
 
+import com.example.fileuploadspringboot.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
+
 public class FileUploadSpringBootApplication {
 
 	public static void main(String[] args) {
